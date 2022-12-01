e5daf92ab567619dfa74256d05ecfc5353a44e1b
jusurkin <61032491+jusurkin@users.noreply.github.com>
2022-11-30 22:24:03 -0500

    Adding next and parsing res parameters

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env mode
 
 import minimist from 'minimist'
-//import * as cli from './lib/a03-jusurkin3/bin/cli.js'
 import { roll } from "./lib/roll.js"
-
-//console.log(roll(6,2,1))
 
 const args = minimist(process.argv.slice(2))
 
@@ -15,24 +12,30 @@ const port = args.port
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/app/', (req, res) => {
+app.get((req, res, next) => {
+  res.status(404)
+  res.send('404 NOT FOUND');
+})
+
+app.get('/app/', (req, res, next) => {
+  res.status(200)
   res.send('200 OK');
 })
 
-app.post('/app/roll/', (req, res) => {
+app.post('/app/roll', (req, res, next) => {
   res.send(roll(6, 2, 1))
 })
 
-app.get('/app/roll/:sides', (req, res) => {
-  res.send(roll(req.params.sides, 2, 1))
+app.post('/app/roll/:sides', (req, res, next) => {
+  res.send(roll(parseInt(req.params.sides), 2, 1))
 })
 
-app.get('/app/roll/:sides/:dice/', (req, res) => {
-  res.send(roll(req.params.sides, req.params.dice, 1))
+app.get('/app/roll/:sides/:dice', (req, res, next) => {
+  res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1))
 })
 
-app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
-  res.send(roll(req.params.sides, req.params.dice, req.params.rolls))
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
+  res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)))
 })
 
 app.listen(port, () => {
