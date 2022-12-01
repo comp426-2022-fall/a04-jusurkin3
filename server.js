eb0b9cd7d070c80e43255dc2965ccc943f78a90c
jusurkin <61032491+jusurkin@users.noreply.github.com>
2022-11-30 19:40:27 -0500

    Endpoint app returns 200 OK

---
+++ server.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env mode
+
+import minimist from 'minimist'
+import * as cli from './lib/a03-jusurkin3/bin/cli.js'
+import { roll } from './lib/a03-jusurkin3/lib/roll.js'
+
+const express = require('express');
+const app = express()
+const port = 5555;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true });
+
+app.get('/app/', (req, res) => {
+  res.send('200 OK');
+})
+
+app.listen(port, () => {
+  console.log("Listening on port " + port)
+})
