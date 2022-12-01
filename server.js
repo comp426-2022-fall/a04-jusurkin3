ff8e54d8c7be1a2c2ddac62f8adbc16ecbcca90f
jusurkin <61032491+jusurkin@users.noreply.github.com>
2022-11-30 20:24:15 -0500

    Attempting endpoints except for the urlencoded part

--- server.js
+++ server.js
@@ -2,17 +2,35 @@
 
 import minimist from 'minimist'
 //import * as cli from './lib/a03-jusurkin3/bin/cli.js'
-//import { roll } from './lib/a03-jusurkin3/lib/roll.js'
+import { roll } from './lib/a03-jusurkin3/lib/roll.js'
+
+const args = minimist(process.argv.slice(2))
 
 import express from 'express'
 const app = express()
-const port = 5555;
+const port = args.port
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/app/', (req, res) => {
   res.send('200 OK');
+})
+
+app.get('/app/roll/', (req, res) => {
+  res.send(roll(6, 2, 1))
+})
+
+app.get('/app/roll/:sides', (req, res) => {
+  res.send(roll(req.params.sides, 2, 1))
+})
+
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+  res.send(roll(req.params.sides, req.params.dice, 1))
+})
+
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+  res.send(roll(req.params.sides, req.params.dice, req.params.rolls))
 })
 
 app.listen(port, () => {
